8c1d27abf39437aeababcc792ab547f978098070
parambuloc <arambulotech@gmail.com>
2024-05-23 12:47:16 -0500

    Change title

--- App One Card/Presentation/Features/Profile/ProfileViewModel.swift
+++ App One Card/Presentation/Features/Profile/ProfileViewModel.swift
@@ -85,7 +85,7 @@ class ProfileViewModel: ProfileViewModelProtocol {
         let documentNumber = userResponse?.docNumber ?? ""
         let companyRUC = userResponse?.companyRUC ?? ""
         
-        verificationRouter.navigateToVerification(email: email, number: number, documentType: documentType, documentNumber: documentNumber, companyRUC: companyRUC, navTitle: "Ingresa tu nuevo correo electrónico", stepDescription: "Paso 1 de 2", operationType: "AE", maskPhoneEmail: true) { [weak self] otpId in
+        verificationRouter.navigateToVerification(email: email, number: number, documentType: documentType, documentNumber: documentNumber, companyRUC: companyRUC, navTitle: "Modifica tu correo electrónico", stepDescription: "Paso 1 de 2", operationType: "AE", maskPhoneEmail: true) { [weak self] otpId in
             self?.router.toEditMail(beforeEmail: email, otpId: otpId) {
                 self?.userResponse?.email = $0
                 if let user = self?.userResponse {
